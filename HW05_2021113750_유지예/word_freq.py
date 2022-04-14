950151bdc9acea7d513deeeccb60b7d4298ef765
YJY1220 <jiyeyu1220@gmail.com>
2022-04-14 20:27:17 +0900

    6th-mkdir dictionary

--- HW05_2021113750_유지예/word_freq.py
+++ HW05_2021113750_유지예/word_freq.py
@@ -8,9 +8,11 @@ param2 = int(sys.argv[2])
 data = open(param1, "rt", encoding='UTF8')
 line = data.readlines()
 
+count = {}
 
 from str_line in line:
     words = str_line.split()
 
     for word in words:
         word = ''.join(filter(str.isalnum,word))
+        count[word] = count.get(word,0) + 1
